5ad0fd3f56ab19cf7b8f110978fbda43e2936fd9
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-08 21:30:47 +0700

    Helo

--- hw2.cpp
+++ hw2.cpp
@@ -5,20 +5,20 @@ int main()
 {
     int n;
 
-    // Nhap n
+    
     cout << "Nhap n: ";
     cin >> n;
 
-    // Bien luu ket qua
+   
     long long factorial = 1;
 
-    // Tinh n!
+   
     for (int i = 1; i <= n; i++)
     {
         factorial = factorial * i;
     }
 
-    // In ket qua
+  
     cout << n << "! = " << factorial;
 
     return 0;
