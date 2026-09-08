bb4594119a645bb7032182807d6fe1c42fbf59b2
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-08 21:37:37 +0700

    Helo

---
+++ hw5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Nhap n: ";
+    cin >> n;
+
+    int reverse = 0;
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+
+        reverse = reverse * 10 + digit;
+
+        n = n / 10;
+    }
+
+    cout << "So dao nguoc = " << reverse;
+
+    return 0;
+}
