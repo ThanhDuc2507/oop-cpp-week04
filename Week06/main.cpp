71e37d9ede41f6d0e09ac57d5569dfc0fede2f3b
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-11 10:20:16 +0700

    Steal egg

---
+++ Week06/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void showWelcome() {
+    cout << "=================================" << endl;
+    cout << "  Welcome to Student Manager!" << endl;
+    cout << "=================================" << endl;
+}
+
+int main() {
+    showWelcome();
+
+    cout << "Let's get started!" << endl;
+    return 0;
+}
